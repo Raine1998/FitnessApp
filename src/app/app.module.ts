303d34cb7a3f816c5397dd4d7f0e4b1324b27ec8
Raine <legaspi.raine@yahoo.com>
2021-10-24 08:06:30 +1300

    install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
+import { Pedometer } from '@ionic-native/pedometer';
+import { Motion } from '@capacitor/motion';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
